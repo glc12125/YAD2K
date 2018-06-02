26689ee2606ce65bcfa0f8d67535fcf08d4b79a7
glc12125 <Liangchuan Gu>
2018-06-02 14:56:53 +0100

    modify yad2k.py to take parse yolo-tiny model

--- test.sh
+++ test.sh
@@ -10,3 +10,5 @@ python3 test_yolo.py --anchors_path model_data/yolo_anchors.txt --classes_path .
 python3 test_yolo.py --anchors_path model_data/yolo_anchors.txt --classes_path model_data/coco_classes.txt --test_path /Users/liangchuangu/Downloads/TASS_jpgs --output_path images/TASS_YOLO_out --score_threshold 0.3 --iou_threshold 0.5 model_data/yolo.h5
 
 python3 test_yolo.py --anchors_path model_data/yolo_anchors.txt --classes_path ../DATA/gtsdb_classes.txt --test_path /Users/liangchuangu/Downloads/Movies/splited/left --output_path images/TASS_out --score_threshold 0.3 --iou_threshold 0.5 retrained_yolo.h5
+
+python3 test_yolo.py --anchors_path model_data/yolo_anchors.txt --classes_path model_data/coco_classes.txt --test_path /Users/liangchuangu/Downloads/TrainIJCNN2013/jpgs --output_path images/out_tiny --score_threshold 0.5 --iou_threshold 0.5 model_data/yolov2-tiny.h5
